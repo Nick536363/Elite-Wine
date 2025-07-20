3121d47f61cbce22ea9cd5635a152b51a49825e8
Nick536363 <3383371nik@gmail.com>
2025-07-20 16:09:07 +0200

    Добавил чтение данных из таблицы и их рендер на сайт

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
-
+from pandas import read_excel
+from pprint import pprint
 
 def word_define(years: int):
     str_year = str(years)
@@ -20,6 +21,21 @@ def word_define(years: int):
         except KeyError:
             return "лет"
 
+
+def get_dict_length(dictionary: dict):
+    params = [param for param in dictionary]
+    return len(dictionary[params[0]])
+
+
+def format_data(wine_number: int, dictionary: dict):
+    data = [dictionary[param][wine_number] for param in dictionary]
+    return data
+
+
+wine_data = read_excel("wine.xlsx").to_dict()
+wines_quantity = get_dict_length(wine_data)
+
+
 env = Environment(
     loader=FileSystemLoader("."),
     autoescape=select_autoescape(["html", "xml"]),
@@ -28,8 +44,11 @@ env = Environment(
 template = env.get_template("template.html")
 
 rendered_page = template.render(
-    years_with_client = datetime.now().year-1923,
-    define_word = word_define(datetime.now().year-1923)
+    years_with_client = datetime.now().year-1920,
+    define_word = word_define(datetime.now().year-1920),
+    wines_quantity = wines_quantity,
+    wine_data = wine_data,
+    format_data = format_data
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
